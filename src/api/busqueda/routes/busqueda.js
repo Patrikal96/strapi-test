2424af466974929b1b4710e1c3065b18c106a38a
Patri <patriperezpua@gmail.com>
2022-12-15 10:39:40 +0100

    strapi-server

---
+++ src/api/busqueda/routes/busqueda.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * busqueda router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::busqueda.busqueda');
---
+++ src/api/busqueda/services/busqueda.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * busqueda service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::busqueda.busqueda');
