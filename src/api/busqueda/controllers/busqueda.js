c945a575dedbe142ad31d8c169f3da40e04b69e8
Patri <patriperezpua@gmail.com>
2022-12-20 10:40:51 +0100

    dale

--- src/api/busqueda/controllers/busqueda.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * busqueda controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::busqueda.busqueda');
--- src/api/busqueda/routes/busqueda.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * busqueda router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::busqueda.busqueda');
